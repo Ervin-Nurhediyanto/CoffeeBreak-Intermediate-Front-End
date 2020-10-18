874c2de3c92b49b82c39baf7b65510b3c4e17e7d
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-10-18 18:39:43 +0700

    Sort

--- src/components/Home/Sort.vue
+++ src/components/Home/Sort.vue
@@ -1,18 +1,22 @@
 <template>
-
   <div class="dropdown">
-    <input type="checkbox" name="" id="" @click="handleShowSort">
+    <input type="checkbox" name="" id="" @click="handleShowSort" />
     <div class="text">Sort</div>
     <span></span>
     <i class="fa fa-caret-down fa-2x"></i>
-      <ol @click="handleShowSort">
-        <li type="submit" @click="handleSort('name')"><a href="#">a-z</a></li>
-        <li type="submit" @click="handleSort('name&order=DESC')"><a href="#">z-a</a></li>
-        <li type="submit" @click="handleSort('price')"><a href="#">low price</a></li>
-        <li type="submit" @click="handleSort('price&order=DESC')"><a href="#">high price</a></li>
-      </ol>
+    <ol @click="handleShowSort">
+      <li type="submit" @click="handleSort('name')"><a href="#">a-z</a></li>
+      <li type="submit" @click="handleSort('name&order=DESC')">
+        <a href="#">z-a</a>
+      </li>
+      <li type="submit" @click="handleSort('price')">
+        <a href="#">low price</a>
+      </li>
+      <li type="submit" @click="handleSort('price&order=DESC')">
+        <a href="#">high price</a>
+      </li>
+    </ol>
   </div>
-
 </template>
 
 <script>
@@ -30,11 +34,7 @@ export default {
     })
   },
   methods: {
-    ...mapActions([
-      'getDataSort',
-      'getAllData',
-      'getData'
-    ]),
+    ...mapActions(['getDataSort', 'getAllData', 'getData']),
     handleSort (sortir) {
       const data = {
         search: this.search,
@@ -57,112 +57,121 @@ export default {
 </script>
 
 <style scoped>
-
-*{
-    margin: 0;
-    padding: 0;
+* {
+  margin: 0;
+  padding: 0;
 }
-.dropdown{
-    width: 150px;
-    margin: 10px auto;
-    position: absolute;
-    z-index: 10000;
-    right: 20px;
+.dropdown {
+  width: 150px;
+  margin: 10px auto;
+  position: absolute;
+  z-index: 10000;
+  right: 20px;
 }
-.dropdown input{
-    position: absolute;
-    width: 100%;
-    height: 50px;
-    z-index: 10000;
-    opacity: 0;
-    cursor: pointer;
+.dropdown input {
+  position: absolute;
+  width: 100%;
+  height: 50px;
+  z-index: 10000;
+  opacity: 0;
+  cursor: pointer;
 }
-.dropdown .text{
-    font-size: 1.1rem;
-    width: 100%;
-    height: 50px;
-    line-height: 50px;
-    transition: all 0.4s;
-    color: black;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
+.dropdown .text {
+  font-size: 1.1rem;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  transition: all 0.4s;
+  color: black;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
-.dropdown i{
-    width: 50px;
-    height: 50px;
-    position: absolute;
-    top: 0;
-    right: 0;
-    color: white;
-    transition: all 0.4s;
-    text-align: center;
-    line-height: 50px;
+.dropdown i {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  color: white;
+  transition: all 0.4s;
+  text-align: center;
+  line-height: 50px;
 }
-.dropdown span{
-    width: 50px;
-    height: 50px;
-    position: absolute;
-    top: 0;
-    right: 0;
-    background-color: black;
-    border-radius: 50px;
-    z-index: -1;
-    transition: all 0.4s;
+.dropdown span {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: black;
+  border-radius: 50px;
+  z-index: -1;
+  transition: all 0.4s;
 }
-.dropdown input:hover ~span{
-    width: 110%;
+.dropdown input:hover ~ span {
+  width: 110%;
 }
-.dropdown input:hover ~ .text{
-    color: white;
+.dropdown input:hover ~ .text {
+  color: white;
 }
-.dropdown input:checked ~ i{
-    transform: rotate(-180deg);
+.dropdown input:checked ~ i {
+  transform: rotate(-180deg);
 }
-.dropdown ol{
-    display: none;
-    list-style: none;
-    margin: 10px 0 0 10px;
+.dropdown ol {
+  display: none;
+  list-style: none;
+  margin: 10px 0 0 10px;
 }
-.dropdown ol li{
-    height: 50px;
-    background-color: black;
-    margin: 0 0 10px 0;
-    cursor: pointer;
-    text-align: center;
-    line-height: 50px;
-    border-radius: 50px;
+.dropdown ol li {
+  height: 50px;
+  background-color: black;
+  margin: 0 0 10px 0;
+  cursor: pointer;
+  text-align: center;
+  line-height: 50px;
+  border-radius: 50px;
 }
-.dropdown ol li a{
-    color: white;
-    text-decoration: none;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
+.dropdown ol li a {
+  color: white;
+  text-decoration: none;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
-.dropdown input:checked ~ ol{
-    display: block;
+.dropdown input:checked ~ ol {
+  display: block;
 }
-.dropdown ol li:hover{
-    background-color: rgb(72, 72, 72);
+.dropdown ol li:hover {
+  background-color: rgb(72, 72, 72);
 }
 
 @media (max-width: 768px) {
-    .dropdown {
-        width: 100px;
-        right: 50px;
-    }
-    .dropdown .text {
-        font-size: 15px;
-        font-weight: bold;
-    }
-    .dropdown ol li{
+  .dropdown {
+    width: 100px;
+    right: 50px;
+  }
+  .dropdown .text {
+    font-size: 15px;
+    font-weight: bold;
+  }
+  .dropdown ol li {
     height: 30px;
-    background-color: black;
-    margin: 0 0 10px 0;
-    cursor: pointer;
-    text-align: center;
     line-height: 20px;
-    border-radius: 50px;
     font-size: 12px;
     padding: 5px;
+  }
+  .dropdown span {
+    width: 30px;
+    height: 30px;
+  }
+  .dropdown i {
+    width: 30px;
+    height: 30px;
+    line-height: 30px;
+    font-size: 16px;
+  }
+  .dropdown .text {
+    font-size: 16px;
+    width: 100%;
+    height: 30px;
+    line-height: 30px;
+  }
 }
-}
-
 </style>
